4f0cd83b11dbee3d8296613aa15a27216c2bc4e1
neitoo <samuraa855@gmail.com>
2024-05-09 18:09:40 +0500

    Реализован компонент хедер

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import rocket from "../assets/rocket.svg";
+import { Auth } from "../context/Auth";
+
+const Header = ({userData}) => {
+    const {handleLogOut} = useContext(Auth);
+
+    return (
+        <header className="bg-white">
+            <div className="shadow-lg p-3 flex items-center">
+                <img className="w-12" src={rocket} alt="Лого" />
+                <div className="px-5">
+                    <p className="text-xl font-semibold leading-6 text-gray-900">{userData.lastName} {userData.firstName} {userData.patronymic}</p>
+                    <p className="text-sm font-semibold leading-6 text-gray-900">Руководитель: {userData.directorId ? userData.directorId : "-"}</p>
+                </div>
+                <div className="lg:flex lg:flex-1 lg:justify-end">
+                    <button href="#" onClick={handleLogOut} className="rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <p className="text-xl font-semibold leading-6">Выйти <span aria-hidden="true">&rarr;</span></p>
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
