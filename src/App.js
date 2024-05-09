711c3bab502d6ac6aea3f9ce07151fe1b3731aba
neitoo <samuraa855@gmail.com>
2024-05-09 18:08:51 +0500

    Реализован роутинг

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useContext } from 'react';
 import './styles/index.css';
+import { Auth } from './context/Auth';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Main from './pages/Main';
+import SignIn from './pages/SignIn';
 
 function App() {
+  const {isUserLogged} = useContext(Auth);
   return (
-    <>
-      <h1 className='text-xl font-bold'>
-        TODO Kosmos
-      </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {isUserLogged ? (
+          <Route path="/tasks" element={<Main/>}/>
+        ) : (
+          <>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          </>
+        )}
+        <Route path="*" element={<Navigate to={isUserLogged ? "tasks" : "sign-in"}/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
