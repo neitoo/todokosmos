c71f65d7763d4b18c1796abbebe532f0354ec0ed
neitoo <samuraa855@gmail.com>
2024-05-09 18:09:24 +0500

    Подключен AuthProvider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './test/reportWebVitals';
+import AuthProvider from './context/Auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
