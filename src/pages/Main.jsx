ee120f3683f6810a383bd9ea0f1689f408a887ec
neitoo <samuraa855@gmail.com>
2024-05-11 17:47:04 +0500

    Перенесен метод получения юзеров

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,21 +4,29 @@ import Header from "../components/Header";
 import TasksList from "../components/TasksList";
 
 const Main = () => {
-    const { handleProtectUser, userData } = useContext(Auth);
+    const { handleProtectUser, handleProtectUsers, userData } = useContext(Auth);
 
     const [hasCalledProtect, setHasCalledProtect] = useState(false);
+    const [protectedUsers, setProtectedUsers] = useState([]);
 
     useEffect(() => {
         if (!hasCalledProtect) {
             handleProtectUser();
+            handleProtectUsers()
+                .then(usersList => {
+                    setProtectedUsers(usersList); 
+                })
+                .catch(error => {
+                    console.error("Ошибка:", error);
+                });
             setHasCalledProtect(true);
         }
-    }, [handleProtectUser, hasCalledProtect]);
+    }, [handleProtectUser, hasCalledProtect,handleProtectUsers]);
 
     return (
         <>
-            <Header userData={userData}/>
-            <TasksList/>
+            <Header userData={userData} users={protectedUsers}/>
+            <TasksList users={protectedUsers}/>
         </>
     );
 }
